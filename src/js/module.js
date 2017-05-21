279cc96e1ec004a9a08d0e75f98c14bfbe9ab7fa
inceon <mr.inceon@gmail.com>
2017-05-22 01:37:36 +0300

    associate back4app and application

--- src/js/module.js
+++ src/js/module.js
@@ -3,10 +3,15 @@
         .module('app', [
             'ui.bootstrap',
             'ui.router',
+            'ngStorage',
+            'ngSanitize',
             'ngCookies',
             'ngAnimate',
             'ngAria',
             'toastr'
         ])
-        .constant('firebase', firebase);
+        .constant('back4app', {
+            appId: 'fPfi2t7V7TqDfJzdUpa220ZsiWbx7FGJbgoR6Mdz',
+            token: 'smwDVqORGP1zeMPeLzlfjOP61dWbbQmK4fPER6HA'
+        });
 }());
